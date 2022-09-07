19758e4fd10f71e6d9c29818e84783772ec1b5ac
Joel Dice <joel.dice@fermyon.com>
2022-09-07 17:00:50 -0600

    add support for CLI commands and spin-config
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- modules/rust/src/main.rs
+++ modules/rust/src/main.rs
@@ -1,4 +1,7 @@
+use anyhow::Result;
+use clap::{Parser, Subcommand};
 use spin_http::{Request, Response};
+use std::{error, fmt};
 
 wit_bindgen_rust::export!("../../wit/spin-http.wit");
 
@@ -26,6 +29,41 @@ impl spin_redis::SpinRedis for SpinRedis {
     }
 }
 
-fn main() {
-    println!("Hello, world!");
+wit_bindgen_rust::import!("../../wit/spin-config.wit");
+
+impl fmt::Display for spin_config::Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Self::Provider(provider_err) => write!(f, "provider error: {}", provider_err),
+            Self::InvalidKey(invalid_key) => write!(f, "invalid key: {}", invalid_key),
+            Self::InvalidSchema(invalid_schema) => {
+                write!(f, "invalid schema: {}", invalid_schema)
+            }
+            Self::Other(other) => write!(f, "other: {}", other),
+        }
+    }
+}
+
+impl error::Error for spin_config::Error {}
+
+#[derive(Debug, Parser)]
+#[clap(author, version, about)]
+pub struct Cli {
+    #[clap(subcommand)]
+    command: Command,
+}
+
+#[derive(Debug, Subcommand)]
+enum Command {
+    Config { key: String },
+}
+
+fn main() -> Result<()> {
+    match &Cli::parse().command {
+        Command::Config { key } => {
+            print!("{}", spin_config::get_config(key)?);
+        }
+    }
+
+    Ok(())
 }
